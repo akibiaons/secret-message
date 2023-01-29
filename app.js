10414e899fdd3fd43a3843661923c5b312890338
Arthur Fernandez <arthur@snooget.com>
2023-01-29 12:16:55 -0800

    just added new string within the array

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ secretMessage.push('to', 'program');
 // Step 2 was to .push() new strings to the array console.log(secretMessage);
 secretMessage[7] = 'right';
 // Step 3 change an element to another string...console.log(secretMessage);
+// Step 4 below will have us remove the first string the array 'Learning'
+secretMessage.shift();
+//console.log(secretMessage);
+// Step 5 below will have us adding the string 'programming' the beginning of the array.
