a255f8c30b15e26833a711b7f1ce9067db46f410
Arthur Fernandez <arthur@snooget.com>
2023-01-29 11:51:15 -0800

    just cause

---
+++ app.js
@@ -0,0 +1,7 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop();
+// Step 1 was to .pop() out the last string of the array console.log(secretMessage);
+secretMessage.push('to', 'program');
+// Step 2 was to .push() new strings to the array console.log(secretMessage);
+secretMessage[7] = 'right';
+// Step 3 change an element to another string...console.log(secretMessage);
